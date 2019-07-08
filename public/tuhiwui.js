8225396cbe614b70646da099176e95d59b88f70a
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-09 09:56:45 +1000

    Hook up electron to display the react content

--- public/tuhiwui.js
+++ public/tuhiwui.js
@@ -15,7 +15,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL('http://localhost:3000')
 
   // Open the DevTools.
   win.webContents.openDevTools()
