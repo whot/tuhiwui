0c99cb51f0dc0b6631c479ad7cc204d8a6840b1e
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-09 10:57:44 +1000

    Add the beginnings of Tuhi DBus bindings

---
+++ public/tuhi.js
@@ -0,0 +1,131 @@
+/* DBus bindings for Tuhi */
+
+"use strict";
+
+var DBus = require('dbus');
+var util = require('util');
+var events = require('events');
+var debug = require('debug')('tuhi');
+var TuhiDevice = require('./tuhidevice');
+
+const BUSNAME_TUHI = 'org.freedesktop.tuhi1'
+const PATH_ROOT = '/org/freedesktop/tuhi1'
+const INTF_MANAGER = 'org.freedesktop.tuhi1.Manager'
+
+var dbus = new DBus();
+
+var Tuhi = module.exports = function() {
+  var self = this;
+
+  self.dbus = dbus;
+  self.sessionBus = dbus.getBus('session');
+  self.systemBus = dbus.getBus('system'); // for BlueZ
+  self.manager = null;
+  self.searching = false;
+  self.devices = {};
+
+  self.unregistered_device_cb = null;
+};
+
+util.inherits(Tuhi, events.EventEmitter);
+
+Tuhi.prototype.init = function(callback) {
+  var self = this;
+
+  self.sessionBus.getInterface(BUSNAME_TUHI, PATH_ROOT, INTF_MANAGER, function(err, iface) {
+    if (err) {
+      console.log('error: ' + err);
+      if (callback)
+        callback(err);
+      return;
+    }
+
+    self.manager = iface;
+
+    iface.getProperty('Devices', function(err, devices) {
+      devices.map(function(d) {
+        var device = new TuhiDevice(self, d);
+        device.init(function(err) {
+          if (err) {
+            console.log('error: ', err);
+            return;
+          }
+
+          self.devices[device.name] = device;
+          self.emit('DeviceAdded', device);
+        });
+      });
+    });
+
+    iface.getProperty('Searching', function(err, value) {
+      self.searching = value;
+    });
+
+    /* FIXME: should be hidden away */
+    self.manager.on('PropertyChanged', function(name, value) {
+      console.log('PropertyChanged: ', name, value);
+      self.emit('PropertyChanged', name, value);
+    });
+
+    self.manager.on('SearchStopped', function(val) {
+      console.log('SearchStopped', val);
+      self.searching = false;
+      self.emit('SearchStopped', value);
+    });
+
+    self.manager.on('UnregisteredDevice', function(objpath) {
+      debug('unregisteredDevice', objpath);
+      if (!self.unregistered_device_cb)
+        return;
+
+      var device = new TuhiDevice(self, objpath);
+      device.init(function(err) {
+        if (err) {
+          console.log('error: ', err);
+          return;
+        }
+        self.unregistered_device_cb(null, device);
+      });
+    });
+
+    if (callback)
+      callback();
+  });
+};
+
+Tuhi.prototype.getProperties = function(callback) {
+  var self = this;
+
+  if (!callback)
+    return;
+
+  self.manager.GetProperties.error = callback;
+  self.manager.GetProperties.finish = function(props) { callback(null, props); };
+  self.manager.GetProperties();
+};
+
+Tuhi.prototype.startSearch = function(callback) {
+  var self = this;
+
+  self.manager.StartSearch(function(err) {
+    if (err) {
+      if (callback)
+        callback(err);
+      return;
+    }
+
+    self.unregistered_device_cb = callback;
+  });
+};
+
+Tuhi.prototype.stopSearch = function(callback) {
+  var self = this;
+
+  self.unregistered_device_cb = null;
+
+  self.manager.StopSearch.error = callback;
+  self.manager.StopSearch.finish = function () {
+    callback(null);
+  };
+  self.manager.StopSearch();
+};
---
+++ public/tuhidevice.js
@@ -0,0 +1,116 @@
+"use strict";
+
+var DBus = require('dbus');
+var util = require('util');
+var events = require('events');
+var debug = require('debug')('tuhi:device');
+
+const BUSNAME_TUHI = 'org.freedesktop.tuhi1'
+const PATH_ROOT = '/org/freedesktop/tuhi1'
+const INTF_DEVICE = 'org.freedesktop.tuhi1.Device'
+
+var TuhiDevice = module.exports = function(tuhi, objpath) {
+  var self = this;
+
+    self.manager = tuhi;
+    self.objectPath = objpath;
+    self.iface = null;
+    self.name = 'unknown';
+    self.register_callback = null;
+};
+
+util.inherits(TuhiDevice, events.EventEmitter);
+
+TuhiDevice.prototype.init = function(callback) {
+  var self = this;
+
+  self.manager.sessionBus.getInterface(BUSNAME_TUHI, self.objectPath, INTF_DEVICE, function(err, iface) {
+    if (err) {
+      if (callback)
+        callback(err);
+      return;
+    }
+
+    self.iface = iface;
+
+    iface.on('PropertyChanged', function(name, value) {
+      console.log("PropertyChanged: ", name, value);
+      self.emit('PropertyChanged', name, value);
+    });
+
+    iface.on('ListeningStopped', function(why) {
+      console.log("ListeningStopped: ", why);
+      self.emit('ListeningStopped', why);
+    });
+
+    iface.on('ButtonPressRequired', function() {
+      debug("ButtonPressRequired signal ");
+
+      if (self.register_cb) {
+        var cb = self.register_cb;
+        self.register_cb = null;
+        cb();
+      }
+    });
+
+    iface.getProperties(function(err, props) {
+      self.listening = props.Listening;
+      self.drawings = props.DrawingsAvailable;
+
+      self.manager.systemBus.getInterface('org.bluez', props.BlueZDevice, 'org.bluez.Device1',
+                                          function(err, bluez_iface) {
+        if (err) {
+        if (callback)
+          callback(err);
+          return;
+        }
+
+        bluez_iface.getProperties(function(err, bluez_props) {
+          self.name = bluez_props.Name;
+          self.address = bluez_props.Address;
+          debug('... bluez device: ', self.name, ' (', self.address, ')');
+
+          // We don't finish init() until we have all properties loaded
+          if (callback)
+            callback(null);
+
+        });
+      });
+    });
+  });
+};
+
+TuhiDevice.prototype.getDrawing = function(timestamp, callback) {
+  var self = this;
+
+  /* I don't know why we can't use the .finish approach here but if we do
+   * so, it just hangs */
+  self.iface.GetJSONData(1, timestamp, function(err, data) {
+    callback(err, data);;
+  });
+};
+
+TuhiDevice.prototype.startListening = function(callback) {
+  var self = this;
+
+  self.iface.StartListening.error = callback;
+  self.iface.StartListening.finish = callback;
+  self.iface.StartListening();
+};
+
+TuhiDevice.prototype.stopListening = function(callback) {
+  var self = this;
+
+  self.iface.StopListening.error = callback;
+  self.iface.StopListening.finish = callback;
+  self.iface.StopListening();
+};
+
+TuhiDevice.prototype.register = function(callback) {
+  var self = this;
+
+  self.register_cb = callback;
+  self.iface.Register.error = callback;
+  self.iface.Register.finish = callback;
+  self.iface.Register();
+};
